af13dd04379b74bb979772f9e9f374c00d4f99fb
Abdomorad <moradabdo127@gmail.com>
2026-02-10 18:11:41 +0200

    last deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,17 +5,17 @@ import path from 'path'
 const app = express()
 
 const port = ENV.PORT
-//const __dirname=path.resolve()
+const __dirname=path.resolve()
 
 app.get('/health', (req, res) => res.send('Hello World!'))
-app.get('/', (req, res) => res.send('Hello!'))
+app.get('/books', (req, res) => res.send('Hello!'))
 
 
 
-/*if (ENV.NODE_ENV=='production'){
+if (ENV.NODE_ENV=='production'){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
     app.get("/{*any}",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
-}*/
+}
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
