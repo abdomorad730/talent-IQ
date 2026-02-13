946c5caa445531ad0fea34cef93d511fe2ad3bf3
Abdomorad <moradabdo127@gmail.com>
2026-02-13 18:49:06 +0200

    2

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,14 @@ import express, { json } from 'express'
 import { ENV } from './lib/env.js'
 import path from 'path'
 import connectDB from './lib/db.js'
+import cors from 'cors'
 
 const app = express()
 
 const port = ENV.PORT
 const __dirname = path.resolve()
-
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 app.get('/health', (req, res) => res.send('Hello World!'))
 app.get('/books', (req, res) => res.send('Hello!'))
 
