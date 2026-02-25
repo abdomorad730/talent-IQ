176d8f1e4a3dfe0d45abddeb547e24b8152d42b2
Abdomorad <moradabdo127@gmail.com>
2026-02-25 06:00:29 +0200

    11

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,21 @@ import AboutPage from "./Pages/AboutPage"
 import ProplemsPage from "./Pages/ProplemsPage"
 import { useUser } from "@clerk/clerk-react"
 import { Toaster } from "react-hot-toast"
+import Dashboard from "./Pages/Dashboard"
 
 function App() {
 
-  const {isSignedIn} = useUser()
+  const {isSignedIn,isLoaded} = useUser()
+  if(!isLoaded) return null
   return (
     <>
       <Routes>
         {/* Define your routes here */}
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={!isSignedIn? <HomePage /> : <Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={isSignedIn ? <Dashboard /> : <Navigate to="/"  />} />
+
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/problems" element={isSignedIn ? <ProplemsPage /> : <Navigate to="/" replace />} />
+        <Route path="/problems" element={isSignedIn ? <ProplemsPage /> : <Navigate to="/"  />} />
       </Routes>
       <Toaster />
     </>
